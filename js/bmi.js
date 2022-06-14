f9f905b8f7258431f6fb7b93a8b449c4366428c9
BOB <godbobkimo@yahoo.com.tw>
2022-06-14 13:50:33 +0800

    完成comment

--- js/bmi.js
+++ js/bmi.js
@@ -12,6 +12,7 @@ function calcBMI() {
     let bmi = getBmi(height, weight);
     if (isNaN(bmi)) {
         bmiResult.innerText = "輸入錯誤!";
+        bmiComment.innerText = " "
         return;
     }
     bmiResult.innerText = bmi;
@@ -19,8 +20,14 @@ function calcBMI() {
         bmiComment.innerText = "太輕了!";
     } else if (bmi >= 18.5 && bmi < 24) {
         bmiComment.innerText = "標準!";
-    } else {
-        bmiComment.innerText = "肥胖!";
+    } else if (bmi >= 24 && bmi < 27) {
+        bmiComment.innerText = "過重!";
+    } else if (bmi >= 27 && bmi < 30) {
+        bmiComment.innerText = "輕度肥胖!";
+    } else if (bmi >= 30 && bmi < 35) {
+        bmiComment.innerText = "中度肥胖!";
+    } else  {
+        bmiComment.innerText = "重度肥胖!"
     }
      console.log(name,height,weight,bmi)
 }
